7b27c4574f1421f7d3915cb1ae36a9860f7c3e20
ModernNeesh <aneesh4942@gmail.com>
2023-05-27 23:12:41 -0700

    PA8 Checkpoint

--- src/CollisionHandling.java
+++ src/CollisionHandling.java
@@ -12,7 +12,10 @@ public class CollisionHandling {
         /*
         TODO: create the values for the hashtable representations
          */
-        int[][] output = null; //change
+        int[][] output;
+        output = new int[2][8];
+        output[0] = new int[]{56, 0, 0, 42, 0, 28, 0, 14};
+        output[1] = new int[]{56, 70, 112, 42, 98, 28, 84, 14};
         return output;
     }
 
@@ -25,7 +28,10 @@ public class CollisionHandling {
         /*
         TODO: create the values for the hashtable representations
          */
-        int[][] output = null; //change
+        int[][] output;
+        output = new int[2][8];
+        output[0] = new int[]{0, 6, 0, 16, 1, 0, 11, 0};
+        output[1] = new int[]{24, 6, 27, 16, 1, 9, 11, 3};
         return output;
     }
 
@@ -38,7 +44,9 @@ public class CollisionHandling {
         /*
         TODO: create the values for the hashtable representations
          */
-        int[][] output = null; //change
+        int[][] output = new int[2][8];
+        output[0] = new int[]{10, 0, 0, 17, 0, 23, 2, 0};
+        output[1] = new int[]{10, 31, 38, 17, 44, 23, 2, 51};
         return output;
     }
 
